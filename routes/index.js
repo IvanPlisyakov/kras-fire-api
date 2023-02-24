49ed6105815f3233bd7b74c4c748f855f696d8b5
IvanPlisyakov <ivan.plisyakov@yandex.ru>
2023-02-24 22:21:56 +0300

    5

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,23 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { login } = require('../controllers/users');
 const { sendOrder } = require('../controllers/orders');
+
 const { getProducts } = require('../controllers/products');
-const { getPhotos } = require('../controllers/photos');
 const routerProducts = require('./products.js');
+
+const { getPhotos } = require('../controllers/photos');
 const routerPhotos = require('./photos.js');
+
+const { getNews }  = require('../controllers/news');
+const routerNews = require('./news.js');
+
+const { getPhotosNews }  = require('../controllers/photosNews');
+const routerPhotoNews = require('./photosNews.js');
+
+const routerDeliverys = require('./deliverys.js');
+
+const routerRefunds = require('./refunds.js');
+
 const { auth } = require('../middlewares/auth.js');
 
 router.post('/signin', celebrate({
@@ -24,6 +37,17 @@ router.use('/products', auth, routerProducts);
 
 router.get('/photos', getPhotos);
 router.use('/photos', auth, routerPhotos);
+
+router.get('/news', getNews);
+router.use('/news', auth, routerNews);
+
+router.get('/photosNews', getPhotosNews);
+router.use('/photosNews', auth, routerPhotoNews);
+
+router.use('/deliverys', auth, routerDeliverys);
+
+router.use('/refunds', auth, routerRefunds);
+
 router.use(/\//, auth);
 
 module.exports = router;
